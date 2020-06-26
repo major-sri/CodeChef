5a49b2347284feb5f1f53a671c23162eb3632a5d
utkarsh srivastava <60793964+major-sri@users.noreply.github.com>
2020-06-26 14:15:36 +0530

    Create COVID19.py

---
+++ COVID19.py
@@ -0,0 +1,15 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    arr.sort()
+    a=[]
+    c=1
+    for x in range(0,n-1):
+        if arr[x+1]-arr[x]<=2:
+            c+=1
+        else:
+            a.append(c)
+            c=1
+    a.append(c)
+    print(min(a),max(a))
