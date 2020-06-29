1659daf8b42298b0d44d868b7ecefcdfd92453b0
utkarsh srivastava <60793964+major-sri@users.noreply.github.com>
2020-06-29 13:24:44 +0530

    Create CORUS.py

---
+++ CORUS.py
@@ -0,0 +1,19 @@
+t=int(input())
+for _ in range(t):
+    n,q=map(int,input().split())
+    a=str(input())
+    arr=list(a)
+    di={}
+    for x in arr:
+        if x in di.keys():
+            di[x]+=1
+        else:
+            di[x]=1
+
+    for i in range(q):
+        case=int(input())
+        cnt=0
+        for x in di.values():
+            if x > case:
+                cnt+=x-case
+        print(cnt)# cook your dish here
