0e8088d4e34fd1583ebae31cbce02e59f667a704
utkarsh srivastava <60793964+major-sri@users.noreply.github.com>
2020-06-30 01:00:36 +0530

    Create WWALK.py

---
+++ WWALK.py
@@ -0,0 +1,19 @@
+t=int(input())
+for _ in  range(t):
+    n=int(input())
+    arr1 = list(map(int, input().split()))
+    arr2 = list(map(int, input().split()))
+    c=0
+    d=0
+    ans=0
+    for i in range(n):
+        if c==d:
+            a=c
+            c+=arr1[i]
+            d+=arr2[i]
+            if c==d:
+                ans+=(c-a)
+        else:
+            c += arr1[i]
+            d += arr2[i]
+    print(ans)
